977b524711e9eb075ddd4be3fc2d1aef04461ee2
Dhanalakshmi-07 <lakshudhana07@gmail.com>
2025-08-22 15:05:04 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,36 @@
+package com.dev;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		int i=10;
+		int sub=1;
+		if(i>0)
+		{
+			System.out.println("The reversed number from 10 to 1 is :");
+			System.out.println(i);
+			System.out.println(sub=i-sub);
+			int val=sub;
+			System.out.println(sub=val-1);
+			int val1=sub;
+			System.out.println(sub=val1-1);
+			int val2=sub;
+			System.out.println(sub=val2-1);
+			int val3=sub;
+			System.out.println(sub=val3-1);
+			int val4=sub;
+			System.out.println(sub=val4-1);
+			int val5=sub;
+			System.out.println(sub=val5-1);
+			int val6=sub;
+			System.out.println(sub=val6-1);
+			int val7=sub;
+			System.out.println(sub=val7-1);		
+		}
+	}		
+	
+}
+			
+
+			
+
